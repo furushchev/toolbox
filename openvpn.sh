9fe0076060bfe8c0391599314f727ff2d6dffbb6
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2017-12-10 16:54:21 +0900

    fix openvpn

--- openvpn.sh
+++ openvpn.sh
@@ -106,7 +106,7 @@ ovpn_init() {
 
   docker volume create --name $OVPN_DATA
   trap error_handler ERR
-  docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u $PROTO://$OVPN_HOSTNAME
+  docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u $PROTO://$OVPN_HOSTNAME:${PORT}
   docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
   docker run -v $OVPN_DATA:/etc/openvpn -d -p ${PORT}:1194/${PROTO} --cap-add=NET_ADMIN kylemanna/openvpn
   prompt info "OpenVPN Server container started"
