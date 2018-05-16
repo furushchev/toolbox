5258289fc09b9d5de02738b1e524c9b4ccc2852d
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2018-05-16 21:27:41 +0900

    update

--- ros/docker_pr2_gazebo/ros_entrypoint.sh
+++ ros/docker_pr2_gazebo/ros_entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 export QT_X11_NO_MITSHM=1
 
 source /ros/kinetic/devel/setup.bash
-# xvfb-run -s "-screen 0 640x480x24"
-exec  "$@"
+xvfb-run -s "-screen 0 640x480x24" "$@"
+#exec  "$@"
--- ros/docker_pr2_gazebo/run.sh
+++ ros/docker_pr2_gazebo/run.sh
@@ -6,13 +6,8 @@ docker run \
     --name test \
     --rm \
     --net=host \
-    --env="DISPLAY" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     furushchev/pr2eus_tutorials:latest \
-    rqt
+    rosrun pr2eus_tutorials pr2_tabletop_sim.sh gui:=false run_rviz:=false --screen
 #    roslaunch pr2_gazebo pr2_empty_world.launch KINECT1:=true gui:=false paused:=false
-#    rosrun pr2eus_tutorials pr2_tabletop_sim.sh gui:=false run_rviz:=false --screen
-
-#    -e QT_X11_NO_MITSHM=1 \
 
 xhost -local:root
