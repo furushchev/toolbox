4d13017c8bf9aa8b0b1f1e6aa5f2a13cd92160a5
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2018-09-17 14:56:41 +0900

    add backup_to_qnap.sh

---
+++ mongo/backup_to_qnap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# lock by pid
+PIDFILE=/var/run/rsync_backup_to_qnap.pid
+if [ -f $PIDFILE ]; then
+    PID=$(cat $PIDFILE)
+    ps -p $PID > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Process already running"
+        exit 1
+    else
+        # forgot to remove PIDFILE?
+        echo $$ > $PIDFILE
+        if [ $? -ne 0 ]; then
+            echo "Could not create PID file at $PIDFILE"
+            exit 1
+        fi
+    fi
+else
+    # no running process
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]; then
+        echo "Could not create PID file at $PIDFILE"
+        exit 1
+    fi
+fi
+
+exec rsync -auvhP /media/mongo/mongodb_store /media/qnap/
+rm -f $PIDFILE
