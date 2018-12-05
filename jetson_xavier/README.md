05e0b1c785470730d80fa2dae96ca93e7ab8c387
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2018-12-05 23:32:57 +0900

    add jetson xavier

---
+++ jetson_xavier/README.md
@@ -0,0 +1,2 @@
+# Use unionfs for Xavier
+
---
+++ jetson_xavier/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update -y -q -qq
+sudo apt-get install -y -q -qq git unionfs-fuse
+
+
+git clone https://github.com/furushchev/toolbox.git /tmp/toolbox
+
+cd /tmp/toolbox/jetson_xavier
+sudo install -m 0755 unionfs-init /usr/sbin/unionfs-init
+sudo mv /sbin/init /sbin/init.bak
+sudo install -m 0755 switch_init /usr/sbin/switch_init
+sudo switch_init
